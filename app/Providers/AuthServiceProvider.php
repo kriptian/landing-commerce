88783430f89e1ab1969ea5f258afe6c11bb89289
cristian ospina <cristian.ospinagarcia@gmail.com>
2025-09-29 22:30:43 -0500

    Ajustes publicos

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,10 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::before(function ($user, $ability) {
+            if ($user->is_admin) {
+                return true;
+            }
+        });
     }
 }
