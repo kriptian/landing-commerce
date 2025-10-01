0405ee223729dd78f20a927e709d51fbbf4f65aa
cristian ospina <cristian.ospinagarcia@gmail.com>
2025-09-30 20:26:46 -0500

    carga pdn redes

--- resources/js/Pages/Public/CartPage.vue
+++ resources/js/Pages/Public/CartPage.vue
@@ -138,7 +138,7 @@ const deleteItem = () => {
     <div v-if="store && (store.facebook_url || store.instagram_url || store.tiktok_url || (store.phone && String(store.phone).replace(/[^0-9]/g,'') ))" class="fixed bottom-6 left-6 z-50">
         <div class="relative">
             <transition name="fade">
-                <div v-if="showSocialFab" class="absolute right-0 bottom-0 flex flex-col items-end gap-3">
+                <div v-if="showSocialFab" class="absolute right-0 bottom-0 flex flex-col items-end gap-3 -translate-y-14 z-10">
                     <a v-if="store.facebook_url" :href="store.facebook_url" target="_blank" class="w-11 h-11 rounded-full bg-white/70 backdrop-blur ring-1 ring-rose-500/50 flex items-center justify-center shadow-2xl active:scale-95">
                         <svg class="w-5 h-5 text-blue-500" viewBox="0 0 24 24" fill="currentColor"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" /></svg>
                     </a>
